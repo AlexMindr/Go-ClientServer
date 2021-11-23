48023663feb112983e34af12dcf952c44b987292
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2021-11-23 21:40:16 +0200

    sub 3 si 5 +fisier

--- cl.go
+++ cl.go
@@ -64,6 +64,56 @@ func subiect3(conn net.Conn, nume string){
 
 }
 
+func arrstr(n int)[]string {
+a := make([]string, n)
+fmt.Println("Enter the inputs")
+for i := 0; i < n; i++ {
+_, err := fmt.Scan(&a[i])
+chkError(err)
+}
+return a
+}
+
+func subiect5(conn net.Conn, nume string){
+
+		p:=[]string{"2dasdas", "12", "dasdas", "1010", "101","a","1","0","-01","111"}
+	/*var n int
+	fmt.Println("Enter the number of elements")
+	_, err14 := fmt.Scan(&n)
+	chkError(err14)
+	p:=arrstr(n)
+	*/
+	encoder := gob.NewEncoder(conn)
+
+	err11 := encoder.Encode(p)
+	chkError(err11)
+
+	var rez []int
+
+	for i:=1;i<5;i++{
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		if message != "" {
+			fmt.Print("Message from server: " + message)
+		}
+		if strings.Contains(message,"Server sends"){
+			re := regexp.MustCompile("[0-9]+")
+			//fmt.Println(re.FindAllString(message, 1))
+			res:=re.FindAllString(message, -1)
+			for _,el :=range res{
+				v:=0
+				v,_=strconv.Atoi(el)
+				rez=append(rez,v)
+
+			}
+			_, err10 := fmt.Fprintf(conn,"Client %s recieved %v as result\n",nume, rez)
+			chkError(err10)
+
+		}
+	}
+
+
+}
+
 func main() {
 
 	fmt.Println("Starting client")
@@ -74,6 +124,13 @@ func main() {
 		log.Fatal("Connection error", err)
 	}
 
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println(message)
+	if strings.Compare(message,"Connected\n")==0{
+
+	}else{
+		os.Exit(1)
+	}
 
 	var nume string
 	scanner := bufio.NewScanner(os.Stdin)
@@ -108,6 +165,8 @@ func main() {
 	switch subiect {
 	case "3":
 		subiect3(conn,nume)
+	case "5":
+		subiect5(conn,nume)
 	default:
 		fmt.Println("Nu exista subiectul")
 	}
--- sv.go
+++ sv.go
@@ -6,13 +6,38 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
 
-type ii struct{
-	val int64
+
+
+var countinst=0
+
+func citireLinii(cale string) ([]string, error) {
+	fisier, err := os.Open(cale)
+	if err != nil {
+		return nil, err
+	}
+
+	//sa astepte ca functiile de mai sus (portiunea de cod) sa se execute corespunzator
+	//inchidem fisierul
+	defer func(fisier *os.File) {
+		err := fisier.Close()
+		checkError(err)
+	}(fisier)
+
+	var linii []string
+	scanner := bufio.NewScanner(fisier)
+	for scanner.Scan() {
+		linii = append(linii, scanner.Text())
+	}
+	return linii, scanner.Err()
 }
+
 
 type clients struct{
 	name string
@@ -41,6 +66,9 @@ func sum(s []int64, c chan int64) {
 	}
 	c <- sum // send sum to c
 }
+
+
+
 
 func checkError(err error) {
 	if err != nil {
@@ -81,6 +109,65 @@ func sub3(conn net.Conn,name string){
 	checkError(err6)
 }
 
+func convert(s string) int64{
+	var res int64
+	res, _ = strconv.ParseInt(s, 2, 64)
+	//fmt.Println(res)
+	return res
+}
+func strtoint(s []string, c chan []int64) {
+
+	var res []int64
+
+	for _, v := range s {
+		re := regexp.MustCompile("^[01]+$")
+		//fmt.Println(re.FindAllString(message, 1))
+		ok:=re.MatchString(v)
+		if ok == true{
+			res=append(res,convert(v))
+
+		}
+	}
+	fmt.Println(res)
+	c <- res // send sum to c
+}
+
+func sub5(conn net.Conn,name string){
+	dec := gob.NewDecoder(conn)
+	var p []string
+	err := dec.Decode(&p)
+	checkError(err)
+	fmt.Printf("Received from %s the following data: %+v\n", name, p)
+	newmessage := "Connected as: "+name
+	_, err2 := fmt.Fprintf(conn, "%s \n", newmessage)
+	checkError(err2)
+	time.Sleep(1000*time.Millisecond)
+	_, err8 := fmt.Fprintf(conn, "%s \n", "Request recieved")
+	checkError(err8)
+
+	//time.Sleep(5000*time.Millisecond)
+	_, err3 := fmt.Fprintf(conn,"%s \n","Server is processing...")
+	checkError(err3)
+
+	time.Sleep(5000*time.Millisecond)
+
+	c := make(chan []int64)
+	go strtoint(p, c)
+	btoint:=<-c
+
+
+	_, err4 := fmt.Fprintf(conn,"Server sends %d as reply \n",btoint)
+	checkError(err4)
+
+	messageres, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Printf("%s\n", messageres)
+
+	err6 := conn.Close()
+	checkError(err6)
+}
+
+
+
 func handleConnection(client clients) {
 	conn:=client.conn
 
@@ -94,20 +181,37 @@ func handleConnection(client clients) {
 	switch subiect{
 	case "3":
 		sub3(conn,name)
-	case "5":\b[01]+\b
+	case "5":
+		sub5(conn,name)
 	default:
 		fmt.Println("Nu exista subiectul!")
 	}
 	fmt.Printf("Closing connection\n")
+	countinst=countinst-1
 }
 
 
 func main() {
 
-	fmt.Println("Starting server")
-	ln, err := net.Listen("tcp", ":8080")
+	//deschide fisierul pentru citire, linie cu linie
+	linii, errf := citireLinii("config.txt")
+	if errf != nil {
+		log.Fatalf("citireLinii: %s", errf)
+	}
+	var file []string
+	// afisam continutul fisierului
+	for _, linie := range linii {
+
+		file=append(file,linie)
+	}
+	port:=file[0]
+	var nrclienti int
+	nrclienti, _ =strconv.Atoi(file[1])
+	fmt.Println("Starting server on port "+port +" max number of clients allowed: " + strconv.Itoa(nrclienti))
+	ln, err := net.Listen("tcp", port)
 	checkError(err)
 	var clienti clients
+
 	// run loop forever (or until ctrl-c)
 	for {
 		conn, err := ln.Accept() // this blocks until connection or error
@@ -116,7 +220,16 @@ func main() {
 		clienti.conn=conn
 
 		checkError(err)
-		go handleConnection(clienti) // a goroutine handles conn so that the loop can accept other connections
-
+		if countinst<nrclienti {
+			_, err := fmt.Fprintf(conn,"Connected\n")
+			checkError(err)
+			time.Sleep(5000*time.Millisecond)
+			go handleConnection(clienti) // a goroutine handles conn so that the loop can accept other connections
+			countinst++
+		}else {
+			_, err := fmt.Fprintf(conn,"Disconnected,server is full\n")
+			checkError(err)
+			fmt.Println("Please upgrade the server!")
+		}
 	}
 }
